ee5fd932c1ec43096acd76835a3705e6a816c51d
ravishankarms-crypto <ravishankar48818@gmail.com>
2023-03-31 17:50:22 +0530

    initial commit

---
+++ bin/datatypes.dart
@@ -0,0 +1,17 @@
+void main()
+{
+ ///variable declaration
+  // name (datatype) -> string
+  // age (datatype) -> integer
+  // mark (datatype) -> double
+  String name ="Anna";
+  int age=8;
+  double mark= 7;
+  String email= "ravi@gmail.com";
+
+  print("Details of the student");
+  print("My name is $name"); //string interpolation
+  print("I am $age years old");
+  print("My  mark is $mark");
+  print("My email id is $email");
+}
